76a1331329d56df369b6b7b08410f303d9980fae
pakarang2505 <156052049+pakarang2505@users.noreply.github.com>
2024-03-25 15:35:34 +0700

    Grocery.java

---
+++ p1/Grocery.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Grocery extends object {
+	JLabel grocery_label=new JLabel("grocery",SwingConstants.CENTER);
+	JLabel grocery_amount=new JLabel("B"+amount,SwingConstants.CENTER);
+	
+	Grocery(Color color){
+		super(Color.CYAN);
+		setBackground(color);
+		setFont(font);
+		this.setLayout(new GridLayout(2,1));
+		this.add(grocery_label);
+		this.add(grocery_amount);
+}
+}
